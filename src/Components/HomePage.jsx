6b40c762145f016e681da1680f4c02f0ee82b4d0
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-09-28 22:49:18 +0530

    v1

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,18 @@
+
+function HomePage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+        <div>
+            <img src="/public/images/dices.png" alt="dice-logo" />
+        </div>
+        <div>
+            <div className="font-bold text-8xl">Dice Game</div>
+            <div className="flex justify-end w-full">
+            <button className="w-52 bg-black text-white rounded-sm" >Play Now</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
