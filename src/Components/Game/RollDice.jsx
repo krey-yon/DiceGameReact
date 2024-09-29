68ab3ea0a7c1162f22f314d9ddfe3c796c56178c
Vikas <101702902+NotYourEren@users.noreply.github.com>
2024-09-29 20:17:05 +0530

    v1

--- src/Components/Game/RollDice.jsx
+++ src/Components/Game/RollDice.jsx
@@ -1,7 +1,12 @@
 
 function RollDice() {
   return (
-    <div>RollDice</div>
+    <div className="flex flex-col justify-center h-full items-center text-center mt-20">
+        <div className="cursor-pointer"><img src="/images/dice_1.png" alt="dice_1" /></div>
+        <div className="font-semibold text-3xl mt-6">Click on Dice to roll</div>
+        <div className="text-xl font-medium border w-60 mt-4 bg-black text-white p-2 rounded-md cursor-pointer">Reset Score</div>
+        <div className="text-xl font-medium border w-60 mt-2 bg-black text-white p-2 rounded-md cursor-pointer">Show Rules</div>
+    </div>
   )
 }
 
